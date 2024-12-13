7607490b2549b919acfbe37de1f1a5a894c27bf2
Raul Garcia Lagunas <rgcoloman@gmail.com>
2024-12-13 16:40:39 -0600

    Consume typed.js CDN and add animation to profession-title

---
+++ resume/assets/js/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#profession-title', {
+    strings: ["Red Teamer", "DevOps Engineer", "Cybersecurity Engineer", "Software Engineer", "Ethical Hacker"],
+    typeSpeed: 100,
+    backSpeed: 110,
+    backDelay: 1000,
+    loop: true
+})
