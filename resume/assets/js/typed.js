d9c7feb391ecda259076256bb71cf7cb4dbb228b
Raul Garcia Lagunas <rgcoloman@gmail.com>
2024-12-16 12:23:13 -0600

    Modify typed.js array

--- resume/assets/js/typed.js
+++ resume/assets/js/typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#profession-title', {
-    strings: ["Red Teamer", "DevOps Engineer", "Cybersecurity Engineer", "Software Engineer", "Ethical Hacker"],
+    strings: ["Red Team Operator", "DevOps Engineer", "Cybersecurity Engineer", "Ethical Hacker", "Software Engineer"],
     typeSpeed: 100,
     backSpeed: 110,
     backDelay: 1000,
