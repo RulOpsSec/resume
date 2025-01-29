5ce2e8b7f539971ae27425ee63059dc5abbe49ba
Raul Garcia Lagunas <rgcoloman@gmail.com>
2025-01-29 17:48:18 -0600

    Modify profession titles and modify experience titles

--- resume/assets/js/typed.js
+++ resume/assets/js/typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#profession-title', {
-    strings: ["Red Team Operator", "DevOps Engineer", "Cybersecurity Engineer", "Ethical Hacker", "Software Engineer"],
+    strings: ["Ethical Hacker", "Software Engineer", "Penetration Tester", "Cybersecurity Specialist"],
     typeSpeed: 100,
     backSpeed: 110,
     backDelay: 1000,
