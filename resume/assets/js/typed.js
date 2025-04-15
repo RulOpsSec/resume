e353129fb209f08f073ebda0ce1b9434526fbdbb
Raul Garcia Lagunas <rgcoloman@gmail.com>
2025-04-15 11:39:51 -0600

    fix download resume button and change typed.js type, back and delay speed

--- resume/assets/js/typed.js
+++ resume/assets/js/typed.js
@@ -1,7 +1,7 @@
 var typed = new Typed('#profession-title', {
-    strings: ["Ethical Hacker", "Software Engineer", "Penetration Tester", "Cybersecurity Specialist"],
-    typeSpeed: 100,
-    backSpeed: 110,
-    backDelay: 1000,
+    strings: ["Security Engineer", "Ethical Hacker", "Software Engineer"],
+    typeSpeed: 90,
+    backSpeed: 100,
+    backDelay: 120,
     loop: true
 })
